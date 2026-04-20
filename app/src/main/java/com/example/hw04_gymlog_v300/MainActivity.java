c0c72f7307506f7dc11a69a8756922c4703bb0bd
IsabelleJ-CSUMB <ijohnson@csumb.edu>
2026-04-19 18:12:46 -0700

    initial setup

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hw04_gymlog_v300;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.hw04_gymlog_v300.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String tag = "GYMLOG";
+    ActivityMainBinding binding;
+    String exercise = "";
+    double weight = 0.0;
+    int reps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
 
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+                updateDisplay();
+            }
+        });
+
+
+    }
+
+    private void updateDisplay() {
+        String currentInfo = binding.logDisplayTextView.getText().toString();
+        String newDisplay = String.format("Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", exercise, weight, reps,currentInfo);
+        binding.logDisplayTextView.setText(newDisplay);
+    }
+    private void getInformationFromDisplay() {
+        exercise = binding.exerciseInputEditText.getText().toString();
+        try {
+            weight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(tag,"Error reading value from weightInputEditText");
+        }
+        try {
+            reps = Integer.parseInt(binding.repInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(tag,"Error reading value from repInputEditText");
+        }
     }
 }

52ad0c5e973b4e468a105e782b2c63aac0437476
IsabelleJ-CSUMB <ijohnson@csumb.edu>
2026-04-24 15:45:46 -0700

    database integration into app

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -12,11 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.hw04_gymlog_v300.database.GymLogRepository;
+import com.example.hw04_gymlog_v300.database.entities.GymLog;
 import com.example.hw04_gymlog_v300.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+    private GymLogRepository repository;
     public static final String TAG = "DAG_GYMLOG";
     String exercise = "";
     double weight = 0.0;
@@ -28,12 +33,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+        repository = GymLogRepository.getRepository(getApplication());
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
 
+        updateDisplay();
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getInformationFromDisplay();
+                insertGymLogRecord();
                 updateDisplay();
             }
         });
@@ -41,22 +49,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void insertGymLogRecord() {
+        if (exercise.isEmpty()) {
+            return;
+        }
+        GymLog log = new GymLog(exercise, weight, reps);
+        repository.insertGymLog(log);
+    }
+
     private void updateDisplay() {
-        String currentInfo = binding.logDisplayTextView.getText().toString();
-        String newDisplay = String.format("Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", exercise, weight, reps,currentInfo);
-        binding.logDisplayTextView.setText(newDisplay);
+        ArrayList<GymLog> allLogs = repository.getAllLogs();
+        if (allLogs.isEmpty()) {
+            binding.logDisplayTextView.setText(R.string.nothing_to_show_time_to_hit_the_gym);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (GymLog log : allLogs) {
+            sb.append(log);
+        }
+        binding.logDisplayTextView.setText(sb.toString());
     }
     private void getInformationFromDisplay() {
         exercise = binding.exerciseInputEditText.getText().toString();
         try {
             weight = Double.parseDouble(binding.weightInputEditText.getText().toString());
         } catch (NumberFormatException e) {
-            Log.d(tag,"Error reading value from weightInputEditText");
+            Log.d(TAG,"Error reading value from weightInputEditText");
         }
         try {
             reps = Integer.parseInt(binding.repInputEditText.getText().toString());
         } catch (NumberFormatException e) {
-            Log.d(tag,"Error reading value from repInputEditText");
+            Log.d(TAG,"Error reading value from repInputEditText");
         }
     }
 }
--- app/src/main/java/com/example/hw04_gymlog_v300/database/GymLogRepository.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/database/GymLogRepository.java
@@ -5,9 +5,7 @@ import android.util.Log;
 
 import com.example.hw04_gymlog_v300.MainActivity;
 import com.example.hw04_gymlog_v300.database.entities.GymLog;
-import com.example.hw04_gymlog_v300.database.entities.GymLogDAO;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -17,24 +15,47 @@ public class GymLogRepository {
     private GymLogDAO gymLogDAO;
     private ArrayList<GymLog> allLogs;
 
-    public GymLogRepository(Application application) {
+    private static GymLogRepository repository;
+
+    private GymLogRepository(Application application) {
         GymLogDatabase db = GymLogDatabase.getDatabase(application);
         this.gymLogDAO = db.gymLogDao();
-        this.allLogs = this.gymLogDAO.getAllRecords();
+        this.allLogs = (ArrayList<GymLog>) this.gymLogDAO.getAllRecords();
+    }
+
+    public static GymLogRepository getRepository(Application application) {
+        if (repository != null) {
+            return repository;
+        }
+        Future<GymLogRepository> future = GymLogDatabase.databaseWriteExecutor.submit(
+                new Callable<GymLogRepository>() {
+                    @Override
+                    public GymLogRepository call() throws Exception {
+                        return new GymLogRepository(application);
+                    }
+                }
+        );
+        try {
+            return future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            Log.i(MainActivity.TAG, "Problemt getting GymLogRepository, thread error");
+        }
+        return null;
     }
 
     public ArrayList<GymLog> getAllLogs() {
-        Future<ArrayList<GymLog>> future  = GymLogDatabase.databaseWriteExecutor.submit(new Callable<ArrayList<GymLog>>() {
+        Future<ArrayList<GymLog>> future  = GymLogDatabase.databaseWriteExecutor.submit(
+                new Callable<ArrayList<GymLog>>() {
             @Override
             public ArrayList<GymLog> call() throws Exception {
-                return gymLogDAO.getAllRecords();
+                return (ArrayList<GymLog>) gymLogDAO.getAllRecords();
             }
         });
         try {
             return future.get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
-            Log.i(MainActivity.TAG, "Problem when getting al Gymlogs in the repo");
+            Log.i(MainActivity.TAG, "Problem when getting al GymLogs in the repo");
         }
         return null;
     }
